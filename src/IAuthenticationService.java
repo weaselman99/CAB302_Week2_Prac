cbcabc045f8fb3131222bb8371a0684b29891b2c
ShiroTohu <shirotohu@shirotohu.dev>
2024-03-06 16:34:23 +1000

    Created interface methods

--- src/IAuthenticationService.java
+++ src/IAuthenticationService.java
@@ -7,8 +7,16 @@ public class AuthenticationService implements IAuthenticationService {
     private ArrayList<User> users;
 
     // TODO Now: Add a constructor to initialize the users list with the default user
+    public AuthenticationService(ArrayList<User> users) {
+        this.users = users;
+    }
 
     // TODO Now: Implement the signUp method to add a new user to the list if the username is not taken and return the user; returns null otherwise
-
+    public User signUp(String username, String password) {
+        throw new UnsupportedOperationException("Feature incomplete. Contact assistance.");
+    }
     // TODO Now: Implement the logIn method to return the user if the username and password match, and null otherwise
+    public User logIn(String username, String password) {
+        throw new UnsupportedOperationException("Feature incomplete. Contact assistance.");
+    }
 }
