dab82ff857f8f53d39732f746350505f14b0266b
ShiroTohu <shirotohu@shirotohu.dev>
2024-03-06 16:22:09 +1000

    Added AuthenticationService interface boiler plate

--- src/IAuthenticationService.java
+++ src/IAuthenticationService.java
@@ -2,3 +2,13 @@ public interface IAuthenticationService {
     User signUp(String username, String password);
     User logIn(String username, String password);
 }
+
+public class AuthenticationService implements IAuthenticationService {
+    private ArrayList<User> users;
+
+    // TODO Now: Add a constructor to initialize the users list with the default user
+
+    // TODO Now: Implement the signUp method to add a new user to the list if the username is not taken and return the user; returns null otherwise
+
+    // TODO Now: Implement the logIn method to return the user if the username and password match, and null otherwise
+}
