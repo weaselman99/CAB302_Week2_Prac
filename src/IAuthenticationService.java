a5603032d375bbe4d0ed97c27e4a794a719f6f22
ShiroTohu <shirotohu@shirotohu.dev>
2024-03-06 16:41:25 +1000

    Implemented signUp method

--- src/IAuthenticationService.java
+++ src/IAuthenticationService.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public interface IAuthenticationService {
     User signUp(String username, String password);
     User logIn(String username, String password);
@@ -13,7 +15,9 @@ public class AuthenticationService implements IAuthenticationService {
 
     // TODO Now: Implement the signUp method to add a new user to the list if the username is not taken and return the user; returns null otherwise
     public User signUp(String username, String password) {
-        return new User(username, password);
+        User user = new User(username, password);
+        this.users.add(user);
+        return user;
     }
     // TODO Now: Implement the logIn method to return the user if the username and password match, and null otherwise
     public User logIn(String username, String password) {
