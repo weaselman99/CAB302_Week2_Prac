d191bfa60a5d7204b179574923c870bb4fe0afd8
Wesley Tuong <wessewly@gmail.com>
2024-03-06 17:00:04 +1000

    work please

--- src/ToDoItem.java
+++ src/ToDoItem.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class ToDoItem {
+public class ToDoItem {
+    private String description;
+    private boolean isDone;
+
+    // TODO Now:
+    // Add a constructor to initialize the item with the description, and isDone as false, with a single parameter for the description
+    // Add getters and setters for each field
+    public ToDoItem(String description) {
+        this.description = description;
+        this.isDone = 0;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public boolean getIsDone() {
+        return this.isDone;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setIsDone(boolean isDone) {
+        this.isDone = isDone;
+    }
 }
